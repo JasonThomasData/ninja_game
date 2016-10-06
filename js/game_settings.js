9d7d44e5ae3d1f041d25aa3ae0a61e70e8268808
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-10-06 23:22:07 +1100

    The game_settings file will contain the defaults for the program

---
+++ js/game_settings.js
@@ -0,0 +1,15 @@
+var game_settings = {
+  player: {
+    starting_hp: 100
+  },
+  enemies: {
+    starting_hp: 50
+  },
+  animation: {
+    frame: {
+      width: 500,
+      height: 300
+    },
+    target_fps: 15
+  }
+}
