dc92b979d72830f8f398b07c1dfa0e694cf3483f
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-10-06 23:01:34 +1100

    Classes for player and enemy objects, very basic for now

---
+++ js/entities.js
@@ -0,0 +1,22 @@
+//This is the class that the player and enemy classes inherit from
+var person = function(px_per_move, px_per_render){
+  this.move_status = 'still' //Can also be running
+  this.direction_facing = 'right' //Can also be left
+  this.falling = false // true if there's no platform underneath
+  this.px_per_move = px_per_move // Minimum move distance in px. Each key press, set this back to its default
+  this.px_per_render = px_per_render //This dictates the speed at which this object moves
+}
+
+//This is our basic player object
+var player = function(px_per_move, px_per_render){
+  person.call(this, px_per_move, px_per_render);
+}
+
+//This will not be part of the MVP
+var enemy = function(px_per_move, px_per_render){
+  person.call(this, px_per_move, px_per_render)
+
+  this.get_enemy_direction = function(){
+    //Here, we want to make the enemy choose its direction based on what direction the player is.
+  }
+}
